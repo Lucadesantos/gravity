7cb63cc8fe1d8a6f0fedf87db3df266aa48791e0
Luca <speedcookie6420@gmail.com>
2022-10-31 13:22:19 +0100

    added zoom feature

--- gravity.py
+++ gravity.py
@@ -6,11 +6,14 @@ import math
 import random
 import pickle 
 
+# Constants
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 SIZE  = (1200,600)
 CENTER = [SIZE[0]/2, SIZE[1]/2]
 LIMIT = (10000,10000)
+SCALE = 1
+
 G = 0.2
 
 toggleVect = False
@@ -44,11 +47,12 @@ def size(v):
 	return abs(math.sqrt(v[0]**2+v[1]**2))
 
 def drawVector(DISPLAY, universe, particle, vect, color, scale):
+	global SCALE
 	centeredPos = addVector(particle.pos, universe.center)
 	vstart = centeredPos
 	vend = addVector(centeredPos, multVector(vect, (scale, scale)))
 
-	pygame.draw.line(DISPLAY, color, vstart, vend, 1)
+	pygame.draw.line(DISPLAY, color, multVector(vstart, (SCALE,SCALE)), multVector(vend,(SCALE,SCALE)), 1)
 
 
 class Universe:
@@ -85,7 +89,7 @@ class Universe:
 			for vector in vectors:
 				normVect = divVector(vector, (particle1.mass**2, particle1.mass**2))				
 				if toggleVect:
-					drawVector(DISPLAY, self, particle1, normVect, (255,0,0), 5000)
+					drawVector(DISPLAY, self, particle1, normVect, (255,0,0), 10000)
 				newVector = addVector(newVector, normVect)
 			particle1.vel = addVector(particle1.vel, newVector)
 			particle1.move()
@@ -146,7 +150,7 @@ class Particle:
 	def move(self):
 		self.pos = addVector(self.pos, self.vel)
 
-def start(n):
+def start(n, m):
 	pygame.init()
 	DISPLAY=pygame.display.set_mode(SIZE,0,32)
 
@@ -156,7 +160,7 @@ def start(n):
 	for i in range(n):
 		x = random.randint(-CENTER[0],CENTER[0])
 		y = random.randint(-CENTER[1],CENTER[1])
-		mass = random.randint(10,200)
+		mass = random.randint(10, m)
 		velX = random.randint(-10, 10)/100
 		velY = random.randint(-10, 10)/100
 		particles.append(Particle(mass, (x,y), (velX, velY)))
@@ -165,7 +169,7 @@ def start(n):
 
 
 def main(DISPLAY, universe):
-	global toggleVect
+	global toggleVect, SCALE
 	pause = False
 	pygame.font.init()
 	font = pygame.font.Font(pygame.font.get_default_font(), 15)
@@ -179,7 +183,7 @@ def main(DISPLAY, universe):
 		numParticles = len(universe.particles)
 		speed = numParticles**(1/10)*0.001/50**(1/10)
 		text_surface = font.render("FPS: " + str(1//speed), False, WHITE)
-		text_surface2 = font.render(str(universe.center), False, WHITE)
+		text_surface2 = font.render(str(universe.center) + " / " + str(round(SCALE,1)), False, WHITE)
 		
 		text_surface3 = font.render("File name: " + text, False, WHITE)
 		
@@ -190,26 +194,29 @@ def main(DISPLAY, universe):
 			DISPLAY.blit(text_surface3, (0,40))
 		keys = pygame.key.get_pressed()
 		if dragging:
-			within.pos = subVector(pygame.mouse.get_pos(), universe.center)
+			pos = multVector(pygame.mouse.get_pos(), (1/SCALE,1/SCALE))
+			within.pos = subVector(pos, universe.center)
+
+
 		time.sleep(speed)
 		for event in pygame.event.get():
 			if event.type==QUIT:
 				pygame.quit()
 				sys.exit()
 			if event.type == pygame.MOUSEBUTTONDOWN:
-				pos1 = pygame.mouse.get_pos()
+				pos1 = multVector(pygame.mouse.get_pos(), (1/SCALE,1/SCALE))
 				within = universe.isIn(subVector(pos1, universe.center))
 				if within:
 					dragging = True
 			if event.type == pygame.MOUSEBUTTONUP:
-				pos2 = pygame.mouse.get_pos()
+				pos2 = multVector(pygame.mouse.get_pos(), (1/SCALE,1/SCALE))
 				if dragging:
 					dragging = False
 					vel = subVector(pos2, pos1)
 					within.vel = divVector(vel, (100,100))
 				else:
 					vel = subVector(pos2, pos1)
-					universe.addParticle(Particle(1000,subVector(pos1, universe.center),multVector(vel, (0.01,0.01))))
+					universe.addParticle(Particle(100000,subVector(pos1, universe.center),multVector(vel, (0.01,0.01))))
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_v:
 					toggleVect = not toggleVect
@@ -230,19 +237,24 @@ def main(DISPLAY, universe):
 					input_active = True
 				
 		if keys[pygame.K_LEFT]:
-			universe.center[0] += 5
+			universe.center[0] += int(5/SCALE)
 		if keys[pygame.K_RIGHT]:
-			universe.center[0] -= 5
+			universe.center[0] -= int(5/SCALE)
 		if keys[pygame.K_UP]:
-			universe.center[1] += 5
+			universe.center[1] += int(5/SCALE)
 		if keys[pygame.K_DOWN]:
-			universe.center[1] -= 5
-
+			universe.center[1] -= int(5/SCALE)
+		if keys[pygame.K_b]:
+			SCALE -= 0.001
+		if keys[pygame.K_n]:
+			SCALE += 0.001
 		for particle in universe.getParticles():
 			centeredPos = addVector(particle.pos, universe.center)
-			pygame.draw.circle(DISPLAY,WHITE,centeredPos,particle.radius)
+			centeredPos = multVector(centeredPos, (SCALE, SCALE))
+			
+			pygame.draw.circle(DISPLAY,WHITE,centeredPos,particle.radius*SCALE)
 			if toggleVect:
-				drawVector(DISPLAY, universe, particle, particle.vel, (0,255,0), 50)
+				drawVector(DISPLAY, universe, particle, particle.vel, (0,255,0), 100)
 			
 		if not pause:
 			universe.update(DISPLAY)
@@ -251,12 +263,15 @@ def main(DISPLAY, universe):
 
 if len(sys.argv)>1:
 	if sys.argv[1].isdigit():
-		DISPLAY, universe = start(int(sys.argv[1]))
+		maxMass = 200
+		if len(sys.argv)>2 and sys.argv[2].isdigit():
+			maxMass = int(sys.argv[2])
+		DISPLAY, universe = start(int(sys.argv[1]), maxMass)
 	else:	
-		DISPLAY, universe = start(0)
+		DISPLAY, universe = start(0, 0)
 		with open("saves/" + sys.argv[1] + ".unv", "rb") as f:
 			universe = pickle.load(f)
 else:
-	DISPLAY, universe = start(100)
+	DISPLAY, universe = start(50, 200)
 
 main(DISPLAY, universe)
